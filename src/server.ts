3ac83d2e3f867c8b0439a3e6c88ebbd88b312380
Golam Kibria <kibriarobin97@gmail.com>
2026-06-22 17:43:36 +0600

    prisma migration and data modeling

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,14 @@ const PORT = config.PORT;
 
 async function main() {
   try {
-    // await prisma.$connect();
+    await prisma.$connect();
     console.log("Connected to the database successfully!");
     app.listen(PORT, () => {
       console.log(`Prisma press server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error("Error starting server:", error);
-    // await prisma.$disconnect();
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
